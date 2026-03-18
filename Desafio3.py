3d0fcfdd1eb8f3bdffdfdb90584850addd3eee35
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-03-18 08:44:22 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,1 +1,6 @@
+# Criando a lista com 5 nomes
+nomes = ["Mayelli", "Fedechen", "Parisotto", "Henrique", "Gustavo"]
 
+# Percorrendo a lista e imprimindo cada nome
+for nome in nomes:
+    print(nome)
