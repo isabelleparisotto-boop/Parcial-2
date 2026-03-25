5a5f95cd9c5f6fae3f231f284491952c3a6a3d6b
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-03-25 08:01:55 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,6 +1,6 @@
-# Criando a lista com 5 nomes
+# Vamos criar uma lista com 5 nomes:
 nomes = ["Mayelli", "Fedechen", "Parisotto", "Amanda", "Ligia"]
 
-# Percorrendo a lista e imprimindo cada nome
+# Agora, vamos imprimir eles em uma lista:
 for nome in nomes:
     print(nome)
