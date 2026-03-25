63fc38b5dfadd8ca185d8cd625edc3e9d739a371
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-03-25 08:03:48 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,9 +1,9 @@
 # Programa que verifica se um número é par
 
-# Pergunta ao usuário que digite um número
+# Perguntamos ao usuário que digite um número
 numero = int(input("Digite um número: "))
 
-# Verifica se o número é par
+# Verificamos se o número é par
 if numero % 2 == 0:
     print(f"O número {numero} é par.")
 else:
