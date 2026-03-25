9c2e53bb207f5dc66f289cfe432aa64faf7be75f
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-03-25 08:49:29 -0300

    Update Desafio5.py

--- Desafio5.py
+++ Desafio5.py
@@ -3,7 +3,7 @@ base = float(input("Digite a base do triângulo: "))
 altura = float(input("Digite a altura do triângulo: "))
 
 # Realiza o cálculo
-area = (base * altura) / 2
+área = (base * altura) / 2
+print (área)
 
-# Exibe o resultado com duas casas decimais
-print(f"A área do triângulo com base {base} e altura {altura} é: {area:.2f}")
+
