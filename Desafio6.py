1f407152d9e392bbf8ac996d2f1a5041b1f2b34f
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-03-31 09:37:01 -0300

    Update Desafio6.py

--- Desafio6.py
+++ Desafio6.py
@@ -1,15 +1,22 @@
-# Entrada do usuário
-total_segundos = int(input("Digite a quantidade de segundos: "))
+# Usuário vai escrever a quantidade de segundos 
+seg = int(input("Digite a quantidade de segundos: "))
 
-#Fazemos o cálculo das horas (divisão inteira por 3600)
-horas = total_segundos // 3600
+# Aqui são os cálculos 
+horas = seg // 3600 
+min = seg // 60 
+# Aqui terá a resposta 
+print("minutos")
+print(min)
+print("horas")
+print(horas)
 
-# Aquilo que sobrar das horas, usamos para calcular os minutos
-resto_segundos = total_segundos % 3600
-minutos = resto_segundos // 60
-
-# Aquilo que sobrar dos minutos são os segundos finais
-segundos_finais = resto_segundos % 60
-
-print(f"{total_segundos} segundos equivalem a:")
-print(f"{horas}h {minutos}min {segundos_finais}s")
+# O programa calculará a quantidade de minutos e segundos através das horas 
+horas1 = int(input("Digite a quantidade de horas: "))
+# Aqui são os cálculos
+min1 = horas1 * 60 
+seg1 = horas * 3600
+# Aqui terá a resposta 
+print("minutos")
+print(min1)
+print("segundos")
+print(seg1)
