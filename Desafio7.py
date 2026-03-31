bc44e2e912bb2ad63c369bce3fda643b8d1d2f58
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-03-31 09:40:04 -0300

    Update Desafio7.py

--- Desafio7.py
+++ Desafio7.py
@@ -1,12 +1,12 @@
 # Entrada de dados
-C = float(input("Digite o capital (C): "))
-I = float(input("Digite a taxa de juros (I) em porcentagem: "))
-T = float(input("Digite o tempo (T): "))
+C = float(input("Digite o capital : "))
+I = float(input("Digite a taxa de juros: "))
+T = float(input("Digite o tempo : "))
 
 # Faremos o cálculo dos juros simples
 # Aplicando a fórmula: J = (C * I * T) / 100
 J = (C * I * T) / 100
 
 # Agora, exibe-se o resultado
-print(f"\nCom um capital de R$ {C:.2f}, taxa de {I}% e tempo de {T},")
-print(f"o valor dos juros (J) será de: R$ {J:.2f}")
+print("O valor do juros é:") 
+print(J)
