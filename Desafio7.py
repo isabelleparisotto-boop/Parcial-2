8bd0115b55c65045adc35deeca6d3da3a976b0f9
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-04-07 09:42:25 -0300

    Update Desafio7.py

--- Desafio7.py
+++ Desafio7.py
@@ -1,4 +1,5 @@
 # Entrada de dados
+# Criamos as variáveis 'C' para capital, 'J' para juros e 'T' para tempo. através de float para números reais e input para entrada de dados 
 C = float(input("Digite o capital : "))
 I = float(input("Digite a taxa de juros: "))
 T = float(input("Digite o tempo : "))
@@ -7,6 +8,6 @@ T = float(input("Digite o tempo : "))
 # Aplicando a fórmula: J = (C * I * T) / 100
 J = (C * I * T) / 100
 
-# Agora, exibe-se o resultado
+# Agora, exibe-se o resultado através de 'print' que é saída de dados
 print("O valor do juros é:") 
 print(J)
