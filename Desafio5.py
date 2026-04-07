b1a4a0f111bfb9608641ad7181cc468646d024e6
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-04-07 09:35:31 -0300

    Update Desafio5.py

--- Desafio5.py
+++ Desafio5.py
@@ -1,9 +1,11 @@
 # Solicita os dados ao usuário
+# Criamos as variáveis 'base' e 'altura' através do 'float' que é utilizado para representar números reais
 base = float(input("Digite a base do triângulo: "))
 altura = float(input("Digite a altura do triângulo: "))
 
 # Realiza o cálculo
 área = (base * altura) / 2
+# ATravés do 'print' terá a saída de dados, ou seja, resultado da área
 print (área)
 
 
