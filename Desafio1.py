ececf5088c880d3ceb35193879d54f67935152d1
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-04-07 09:22:09 -0300

    Update Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,6 +1,9 @@
- # Sistema que realiza a soma entre dois números 
+# Sistema que realiza a soma entre dois números 
+# Faremos as variáveis, o int é para números inteiros, input é para entradas de dados 
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
+# Agora,só realizar a soma 
 soma = n1 + n2
+# O print é para exibir os dados, a saída deles
 print("o resultado é:")
 print(soma)
