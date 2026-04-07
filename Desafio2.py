0e4d4e807b1205dbe1e22520a8260bbdcb2a6204
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-04-07 09:25:23 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,8 +1,10 @@
-
 # Perguntamos ao usuário que digite um número
+# Fazemos a pergunta 
+# Através de int que é para números inteiros e o input que é para entrada de dados
 numero = int(input("Digite um número: "))
 
 # Verificamos se o número é par
+# O if e o else são para e se o número for divisível por 2 ele é par, se não ele é ímpar
 if numero % 2 == 0:
     print("O número é par.")
 else:
