0d9c94f570fbac082507a049ee51b6ae803ae045
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-04-07 09:26:42 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,11 +1,12 @@
 # Pediremos ao usuário que liste 5 nomes 
+# Criamos as variáveis com as perguntas através do input que é para entrada de dados 
 nome = input("Digite um nome: ")
 nome1 = input("Digite um nome: ")
 nome2 = input("Digite um nome: ")
 nome3 = input("Digite um nome: ")
 nome4 = input("Digite um nome: ")
 
-# Agora o programa lista os nomes dados em uma lista
+# Agora o programa lista os nomes dados em uma lista. através do print que é para saída de dados
 
 print("Sua lista ficou assim: ")
 print(nome, nome1, nome2, nome3, nome4)
