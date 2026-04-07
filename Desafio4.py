1e73fae844e24f355e8e0ef2a571a299d61bc295
isabelleparisotto-boop <isabelle.parisotto@escola.pr.gov.br>
2026-04-07 09:33:34 -0300

    Update Desafio4.py

--- Desafio4.py
+++ Desafio4.py
@@ -1,4 +1,5 @@
-# Solicite os números ao usuário 
+# Solicitamos os números ao usuário 
+# Através do float que representa números reais e input que é a entrada de dados criamos as variáveis
 num1 = float(input("Escolha um número "))
 num2 = float(input("Escolha outro número "))
 
@@ -7,18 +8,18 @@ operação = input("Escolha uma operação [soma, subtração, divisão ou multi
 
 #Vamos realizar as operações 
 
-# Raliza a operação de soma
+# Raliza a operação de soma, através do if que significa 'se'
 if operação == "soma":
     print(num1 + num2)
-# Realiza a subtração
+# Realiza a subtração, através do elif que é uma abreviação para 'else if' que significa 'senão se' 
 elif operação == "subtração":
     print(num1-num2)
-# Realiza a multiplicação
+# Realiza a multiplicação, através do elif que é uma abreviação para 'else if' que significa 'senão se' 
 elif operação == "multiplicação":
     print(num1*num2)
-# Realiza a divisão
+# Realiza a divisão, através do elif que é uma abreviação para 'else if' que significa 'senão se' 
 elif operação == "divisão":
     print(num1/num2)
-# Se for uma operaçãop que não existe diga 'esse número não existe'
+# Se for uma operação que não existe diga 'esse número não existe' através do 'else' que significa uma variavel caso os comandos sejam contrários aos anteriores
 else:
     print('esse numero não existe')
